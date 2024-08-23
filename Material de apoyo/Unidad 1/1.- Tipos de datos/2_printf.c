9d6ba4e13401695b3dc025118da060b5b192daa4
Matías González Belmar <matias.gonzalez1901@alumnos.ubiobio.cl>
2024-08-22 21:50:01 -0400

    Update 2_printf.c

--- Material de apoyo/Unidad 1/1.- Tipos de datos/2_printf.c
+++ Material de apoyo/Unidad 1/1.- Tipos de datos/2_printf.c
@@ -3,11 +3,11 @@
 /*
     Ya sabemos que existen varios tipos de datos en el lenguaje C, pero una cosa importante es saber que existen
     y otra también bastante importante es poder desplegar estos valores en la terminal y así poder realizar 
-    corraboraciones de los datos o simplemente un seguimiento de que esté todo como debería.
+    corroboraciones de los datos o simplemente un seguimiento de que esté todo como debería.
 */
 
 int main(){
-    // Tomemos de ejemplo los tipos de datos : Int , Float , un Char y una cadena de Chars
+    // Tomemos de ejemplo los tipos de datos: int, float, char y una cadena de chars
     int entero = 4;
     float decimal = 4.15;
     char letra = 'A';
@@ -18,15 +18,15 @@ int main(){
     |---> printf() 
     
     La sintaxis de esta función es la siguiente: 
-    |-> printf( "%d,f,s,c,etc" ,   nombre de la variable ); 
+    |-> printf( "%d, %f, %s, %c", nombre de la variable ); 
 
-    Si te diste cuenta introduje un % seguido de una letra (d,f,s,c,etc), estas letras no están por qué sí
-    sino que sirven para indicar al programa en que parte del texto ingresar la variable dentro del printf 
+    Si te diste cuenta, introduje un % seguido de una letra (d, f, s, c, etc.), estas letras no están por qué sí
+    sino que sirven para indicar al programa en qué parte del texto ingresar la variable dentro del printf 
     a la par que define el tipo de dato que vamos a desplegar. 
 
-    la correspondencia de las letras sería la siguiente:
+    La correspondencia de las letras sería la siguiente:
     |-> %d : Tipo de dato entero
-    |-> %s : Tipo de dato string
+    |-> %s : Tipo de dato cadena de caracteres
     |-> %f : Tipo de dato flotante 
     |-> %c : Tipo de dato char
     */
@@ -34,10 +34,11 @@ int main(){
     printf("Este es un dato del tipo entero: %d\n", entero);
     printf("Este es un dato del tipo flotante: %f\n", decimal);
     printf("Este es un dato del tipo char: %c\n", letra);
-    printf("Este es un dato del tipo string: %s\n", cadena);
+    printf("Este es un dato del tipo cadena de caracteres: %s\n", cadena);
 
-    // Tambien puedes notar que agregué  "\n" al final de cada printf, esto es para marcar un salto de linea
-    // así evitamos que se imprima todo junto entre un printf al otro.
+    // También puedes notar que agregué "\n" al final de cada printf, esto es para marcar un salto de línea
+    // así evitamos que se imprima todo junto entre un printf y el otro.
     
     return 0;
 }
+
